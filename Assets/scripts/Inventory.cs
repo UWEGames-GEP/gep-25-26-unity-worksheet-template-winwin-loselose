5a65eb52c786ce49d6d1f90a8b915c54f2e429b7
mlem <183514108+winwin-loselose@users.noreply.github.com>
2025-10-08 15:18:00 +0100

    inventory basics in, no more errors!

--- Assets/scripts/Inventory.cs
+++ Assets/scripts/Inventory.cs
@@ -2,16 +2,22 @@ using UnityEngine;
 using System.Collections.Generic;
 public class Inventory : MonoBehaviour
 {
-    pubilc List<string> items = new List<string>();
+    public List<string> items = new List<string>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+            addItem("fuck");
+        if (Input.GetKeyDown(KeyCode.T))
+            removeItem("fuck");
+    }
+    public void addItem(string itemName)
+    {
+        items.Add(itemName);
+    }
+    public void removeItem(string itemName)
+    {
+        items.Remove(itemName);
     }
 }

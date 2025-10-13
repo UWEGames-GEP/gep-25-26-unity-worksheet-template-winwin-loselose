3705bdd58ab8b66ee3cadb8a221570112f1cafa9
mlem <183514108+winwin-loselose@users.noreply.github.com>
2025-10-13 16:04:01 +0100

    added item visuals on inventory and removal!

--- Assets/scripts/Inventory.cs
+++ Assets/scripts/Inventory.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 using System.Collections.Generic;
 using static GameManager;
+using TMPro;
+using System.Linq;
 public class Inventory : MonoBehaviour
 {
     public List<string> items = new List<string>();
     [SerializeField]GameManager game_manager;
     [SerializeField]bool can_add_item = false;
+    [SerializeField] GameObject[] inventory_slots;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         //finds the game manager in the scene
         game_manager = GameObject.FindAnyObjectByType<GameManager>();
+        inventory_slots = GameObject.FindGameObjectsWithTag("inventory");
+        sortItems();
     }
     private void LateUpdate()
     {
@@ -24,7 +29,7 @@ public class Inventory : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R))
             addItem("fuck");
         if (Input.GetKeyDown(KeyCode.T))
-            removeItem("fuck");
+            removeLastItemDebug();
     }
 
     //voids for adding and removing items to be accessed by other scripts
@@ -32,10 +37,32 @@ public class Inventory : MonoBehaviour
     {
         if(can_add_item)
             items.Add(itemName);
+        sortItems();
     }
     public void removeItem(string itemName)
     {
         if(can_add_item)
             items.Remove(itemName);
+        sortItems();
+    }
+    public void removeLastItemDebug()
+    {
+        if (can_add_item)
+            items.Remove(items.Last());
+        sortItems();
+    }
+    public void sortItems()
+    {
+        items.Sort();
+        //clear all inventory names before setting new#
+        for (int i = 0; i < inventory_slots.Length; i++)
+        {
+            inventory_slots[i].GetComponent<TMP_Text>().SetText("");
+        }   
+        //set inventory text/icons
+        for (int i = 0; i < items.Count; i++)
+        {
+            inventory_slots[i].GetComponent<TMP_Text>().SetText(items[i].ToString());
+        }
     }
 }
